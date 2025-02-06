fe1e16f9c7536721d9400a8f3590705aa554be3d
marcosvallecillos <mavaus@floridauniversitaria.es>
2025-02-06 09:57:01 +0100

    Primer commit

---
+++ tests/DNITest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\DNI;
+
+class DNITest extends TestCase {
+    public function testCalculoDNIConLetra() {
+        $dni = new DNI(12345678); 
+        $this->assertEquals("12345678Z", $dni->getDNIConLetra()); 
+    }
+}
